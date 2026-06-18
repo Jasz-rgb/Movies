d5fdfc8f9a44e1210cfbda31b7a43fdd068748fb
Jasz-rgb <jasminesahoo007@gmail.com>
2026-06-19 04:27:26 +0530

    slider and .gitignore updated

--- frontend/src/components/ui/slider.tsx
+++ frontend/src/components/ui/slider.tsx
@@ -35,12 +35,10 @@ function Slider({
       {...props}
     >
       <SliderPrimitive.Track
-        data-slot="slider-track"
-        className="relative grow overflow-hidden rounded-full bg-muted data-horizontal:h-1.5 data-horizontal:w-full data-vertical:h-full data-vertical:w-1.5"
+        className="relative grow overflow-hidden rounded-full bg-gray-300 data-horizontal:h-1.5 data-horizontal:w-full data-vertical:h-full data-vertical:w-1.5"
       >
         <SliderPrimitive.Range
-          data-slot="slider-range"
-          className="absolute bg-primary select-none data-horizontal:h-full data-vertical:w-full"
+          className="absolute bg-orange-400 select-none data-horizontal:h-full data-vertical:w-full"
         />
       </SliderPrimitive.Track>
       {Array.from({ length: _values.length }, (_, index) => (
